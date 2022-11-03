14d20532acaca6c50ff3a223e5413a841f3cc47c
unknown <meliodas_17@hotmail.com>
2022-11-03 12:02:22 -0300

    cleaning up images

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTypes } from "../redux/actions";
@@ -13,43 +13,70 @@ export const Form = ()=>{
         if(!types.length) dispatch(getTypes())
     },[])
 
+    const [info, setInfo] = useState({
+        name:"",
+        image:"", 
+        id:"", 
+        hp:"",
+        attack:"",
+        defense:"",
+        specialAttack:"",
+        specialDefense:"",
+        speed:"",
+        types:[]            
+    })
+
+    const handleChange = (e)=>{
+        console.log(info)
+        if(e.target.name === "types"){
+            info.types.includes(e.target.value)? setInfo({...info,
+                 types: info.types.filter(a=> a !== e.target.value)}):
+                 setInfo({...info,
+                types: info.types.concat(e.target.value)})
+                return     
+        }
+        setInfo({...info,
+        [e.target.name]:e.target.value})
+    }
+
+
     return(
         <main className={style.main}>
             <form className={style.form}>
                 <div className={style.itemContainer}>
                     <div className={style.item}>
-                        <label htmlFor="Name">Name</label>
-                        <input className={style.input} for="Name" type="text"></input>
+                        <label htmlFor="name">Name</label>
+                        <input onChange={handleChange} className={style.input} name="name" type="text" value={info.name}></input>
                     </div>
                     <div className={style.item}>
-                        <label htmlFor="Image">Image</label>
-                        <input className={style.input} for="Image" type="text"></input>
+                        <label htmlFor="image">Image</label>
+                        <input onChange={handleChange} className={style.input} name="image" type="text" value={info.image}></input>
                     </div>
                 </div>
                 <div className={style.itemContainer}>
                 <div className={style.item}>
                         <label htmlFor="hp">HP</label>
-                        <input className={style.input} name="hp" type="range"></input>
+                        <input onChange={handleChange} className={style.input} name="hp" type="range" value={info.hp} ></input>
                     </div>
                     <div className={style.item}>
-                        <label htmlFor="DF">DF</label>
-                        <input className={style.input} name="DF" type="range"></input>
+                        <label htmlFor="df">DF</label>
+                        <input onChange={handleChange} className={style.input} name="df" type="range" value={info.defense}></input>
                     </div>
                     <div className={style.item}>
-                        <label htmlFor="ATK">ATK</label>
-                        <input className={style.input} name="ATK" type="range"></input>
+                        <label htmlFor="attack">ATK</label>
+                        <input onChange={handleChange} className={style.input} name="attack" type="range" value={info.attack}></input>
                     </div>
                     <div className={style.item}>
-                        <label htmlFor="SPA">SPA</label>
-                        <input className={style.input} name="SPA" type="range"></input>
+                        <label htmlFor="specialAttack">SPA</label>
+                        <input onChange={handleChange} className={style.input} name="specialAttack" type="range" value={info.specialAttack}></input>
                     </div>
                     <div className={style.item}>
-                        <label htmlFor="SPD">SPD</label>
-                        <input className={style.input} name="SPD" type="range"></input>
+                        <label htmlFor="specialDefense">SPD</label>
+                        <input onChange={handleChange} className={style.input} name="specialDefense" type="range" value={info.specialDefense}></input>
                     </div>
                     <div className={style.item}>
-                        <label htmlFor="Speed">Speed</label>
-                        <input className={style.input} name="Speed" type="range"></input>
+                        <label htmlFor="speed">Speed</label>
+                        <input onChange={handleChange} className={style.input} name="speed" type="range" value={info.speed}></input>
                     </div>
                 </div>
                 <div className={style.item}>
@@ -59,7 +86,7 @@ export const Form = ()=>{
                                     <div key={g.name} className={style.itemCheck}>
                                         <label key={g.name + 1} className={style.checkLabel} htmlFor={g.name}>{g.name}
                                         </label>
-                                        <input  name ={g.name} type="checkbox" value={g.name} className={style.check}  key={i}>
+                                        <input onClick={handleChange} name ="types" type="checkbox" value={g.name} className={style.check}  key={i}>
                                         </input>
                                     </div>
                                         )   
