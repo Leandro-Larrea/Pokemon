0d46bdfae85033ab4a5806a7def5e341bf559d34
unknown <meliodas_17@hotmail.com>
2022-12-17 02:59:55 -0300

    acomodando el port

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { conn } = require("./src/db.js")
 
 conn.sync({force:false}).then(()=>{
-server.listen(process.env.port || 3001, ()=>{
+server.listen(process.env.PORT || 3001, ()=>{
     console.log("listening the best server in the world =D")
         });
     });
