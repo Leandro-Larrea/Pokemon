50e2f3b5f41b484aa15309df404a996af36a5095
Leandro <leandrodevlarrea@hotmail.com>
2023-01-20 09:35:21 -0300

    reset server

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { conn } = require("./src/db.js")
 const port = process.env.PORT || 3001;
 
-conn.sync({force:false}).then(()=>{
+conn.sync({force:true}).then(()=>{
 server.listen( port , ()=>{
         });
     });
